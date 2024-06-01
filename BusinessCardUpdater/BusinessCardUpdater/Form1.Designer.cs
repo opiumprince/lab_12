1dec841fdf9ebb663e79a104155364b0e9e96f82
opiumprince <128786059+opiumprince@users.noreply.github.com>
2024-06-01 14:26:54 +0100

    Add files via upload

---
+++ BusinessCardUpdater/BusinessCardUpdater/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BusinessCardUpdater
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Освобождаем все используемые ресурсы
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnUpdateCard = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(100, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(100, 60);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(100, 95);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(200, 20);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(100, 130);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // btnUpdateCard
+            // 
+            this.btnUpdateCard.Location = new System.Drawing.Point(100, 165);
+            this.btnUpdateCard.Name = "btnUpdateCard";
+            this.btnUpdateCard.Size = new System.Drawing.Size(200, 30);
+            this.btnUpdateCard.TabIndex = 4;
+            this.btnUpdateCard.Text = "Update Business Card";
+            this.btnUpdateCard.UseVisualStyleBackColor = true;
+            this.btnUpdateCard.Click += new System.EventHandler(this.btnUpdateCard_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(30, 28);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(30, 63);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(30, 98);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 7;
+            this.lblPhone.Text = "Phone";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(30, 133);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(334, 211);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnUpdateCard);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Business Card Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnUpdateCard;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
---
+++ BusinessCardUpdater/BusinessCardUpdater/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace BusinessCardUpdater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpdateCard_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string title = txtTitle.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+
+            string templatePath = @"C:\Users\Bohdan\Desktop\test.dotm";
+
+            UpdateBusinessCard(templatePath, name, title, phone, email);
+        }
+
+        private void UpdateBusinessCard(string templatePath, string name, string title, string phone, string email)
+        {
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = null;
+
+            try
+            {
+                doc = wordApp.Documents.Open(templatePath);
+
+                ReplaceText("{Name}", name, doc);
+                ReplaceText("{Title}", title, doc);
+                ReplaceText("{Phone}", phone, doc);
+                ReplaceText("{Email}", email, doc);
+
+                doc.Save();
+                MessageBox.Show("Візитку успішно оновлено!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка під час оновлення візитки: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                doc.Close();
+                wordApp.Quit();
+            }
+        }
+
+        private void ReplaceText(string placeholder, string newValue, Word.Document doc)
+        {
+            foreach (Word.Range range in doc.StoryRanges)
+            {
+                Word.Find find = range.Find;
+                find.Text = placeholder;
+                find.Replacement.Text = newValue;
+                find.Execute(Replace: Word.WdReplace.wdReplaceAll);
+            }
+        }
+    }
+}
